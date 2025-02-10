24aad860584de6a4395819fc829236e3338d62b0
ansteisija-kat <katty93cat@gmail.com>
2025-02-10 16:42:29 +0300

    add new test & var in SERP class for asserting secondary class

--- page_object/seachResultsPage.js
+++ page_object/seachResultsPage.js
@@ -10,27 +10,40 @@ export class SERP {
     //  */
     // без этого заработало, с этим импорт не работает ??
 
+    activeTab = /Tabs__tab_active--/  // вспомогательный класс, не бывает единственным
+
     constructor(page) {
         this.page = page;
 
         this.destinationBlock = page.locator('.zenregioninfo').first();
         this.destinationBlockCity = page.locator('p.zenregioninfo-region').first();
         this.destinationBlockDates = page.locator('p.zenregioninfo-dates').first();
+        this.destinationBlockDatesText = (page.locator('p.zenregioninfo-dates').first()).innerText();
         this.destinationBlockGuestRooms = page.locator('p.zenregioninfo-rooms').first();
         this.destinationBlockEditIcon = page.locator('.zenregioninfo-lens').first(); // лупа
 
-        this.tabAll = page.getByText('All');
-        this.tabHotels = page.getByText('Hotels');
-        this.tabAparts = page.getByText('Apartments');
+        this.tabAll = page.locator('//*[contains (@class, "Tabs__tab--")]').filter({ hasText: 'All' });
+        this.tabHotels = page.locator('//*[contains (@class, "Tabs__tab--")]').filter({ hasText: 'Hotels' });
+        this.tabAparts = page.locator('//*[contains (@class, "Tabs__tab--")]').filter({ hasText: 'Apartments' });
         this.title = page.locator('p.zenserpresult-header');
         this.hotelsList = page.locator('.hotels').first();
+        this.emptyResults = page.locator('.emptyserpfiltered');
+        this.emptyResultsTitle = page.locator('.emptyserpfiltered-title');
+        this.emptyResultsText = page.locator('.emptyserpfiltered-text');
+        this.emptyResultsFilters = page.locator('.emptyserpfiltered-filters');
+        this.emptyResultsFilterRemoveButton = page.locator('.appliedfilter-remove');
+
+        // leftbar
+        this.fav = page.locator('.zen-hotels-filter-favorites');
 
     }
 
-    // async goto() {
-    //     await this.page.goto(searchResultsPageUrl);
+    async goto(url) {
+        await this.page.goto(url);
+    }
+
+    // async getDatesFromDestinationBlock() {
+    //     return this.destinationBlockDates.innerText()
     // }
-
-
 
 }
--- tests/seachResultsPage.test.js
+++ tests/seachResultsPage.test.js
@@ -1,15 +1,65 @@
 import { test, expect } from '@playwright/test';
-import {SERP} from "../page_object/seachResultsPage.js";
+import { SERP } from "../page_object/seachResultsPage.js";
 
 
-test("main elements on search results page", async ({ page }) => {
+// const activeTab = /Tabs__tab_active--/
+
+// test("main elements on page", async ({ page }) => {
+//     const SearchPage = new SERP(page);
+//     await SearchPage.goto()
+//
+//     await SearchPage.destinationInput.fill('Prague');
+//
+//     await expect(SearchPage.title).toBeVisible()
+//     await expect(SearchPage.checkinInput).toHaveText('Feb 8, 2025');
+//     await expect(SearchPage.checkoutInput).toHaveText('Feb 9, 2025');
+//     await expect(SearchPage.guests).toHaveText('2 guests');
+// });
+
+test("get dates from destination block", async ({ page }) => {
     const SearchPage = new SERP(page);
-    await SearchPage.goto()
+    await SearchPage.goto('https://ostrovok.ru/hotel/russia/moscow/?q=2395&dates=15.02.2025-16.02.2025&guests=2&trip_type=business_trip&price=one&sid=1ee79317-7c74-4c6d-8822-4b3261bcd5e4')
 
-    await SearchPage.destinationInput.fill('Prague');
-
-    await expect(SearchPage.title).toBeVisible()
-    await expect(SearchPage.checkinInput).toHaveText('Feb 8, 2025');
-    await expect(SearchPage.checkoutInput).toHaveText('Feb 9, 2025');
-    await expect(SearchPage.guests).toHaveText('2 guests');
+    await expect(SearchPage.title).toBeVisible();
+    // console.log('destinationBlockDatesText :', SearchPage.destinationBlockDatesText);
+    // console.log('destinationBlockDates :', SearchPage.destinationBlockDates);
+    await expect(SearchPage.destinationBlockDates).toHaveText('15 Feb 2025 — 16 Feb 2025');
 });
+
+test("get a city in a title and destination block", async ({ page }) => {
+    const SearchPage = new SERP(page);
+    await SearchPage.goto('https://ostrovok.ru/hotel/russia/moscow/?q=2395&dates=15.02.2025-16.02.2025&guests=2&trip_type=business_trip&price=one&sid=1ee79317-7c74-4c6d-8822-4b3261bcd5e4')
+
+    await expect(SearchPage.destinationBlockCity).toHaveText('Moscow, Russia');
+    await expect(SearchPage.title).toContainText('Moscow'); // 2 заголовка – о поиске, пока загрузка, и результатах
+});
+
+test("empty results list if favourites active", async ({ page }) => {
+    const SearchPage = new SERP(page);
+    await SearchPage.goto('https://ostrovok.ru/hotel/russia/moscow/?q=2395&dates=15.02.2025-16.02.2025&guests=2&trip_type=business_trip&price=one&sid=1ee79317-7c74-4c6d-8822-4b3261bcd5e4')
+
+    // let titleBefore = SearchPage.title.innerText()
+    await SearchPage.fav.click();
+    // let titleAfter = SearchPage.title.innerText()
+    // console.log('titleBefore: ', titleBefore);
+    // console.log('titleAfter: ', titleAfter);
+    await expect(SearchPage.emptyResults).toBeVisible();
+    await expect(SearchPage.emptyResultsFilters).toBeVisible();
+    await expect(SearchPage.emptyResultsFilterRemoveButton).toBeEnabled();
+});
+
+test("click on tab remove filters and get new results list", async ({ page }) => {
+    const SearchPage = new SERP(page);
+    await SearchPage.goto('https://ostrovok.ru/hotel/russia/moscow/?q=2395&dates=15.02.2025-16.02.2025&guests=2&trip_type=business_trip&price=one&sid=1ee79317-7c74-4c6d-8822-4b3261bcd5e4')
+
+    await SearchPage.fav.click();
+    await expect(SearchPage.emptyResults).toBeVisible();
+
+    await SearchPage.tabHotels.click();
+    await expect(SearchPage.emptyResults).not.toBeVisible();
+    await expect(SearchPage.title).toContainText('Moscow');
+    await expect(SearchPage.tabHotels).toHaveClass(SearchPage.activeTab);
+});
+
+
+
