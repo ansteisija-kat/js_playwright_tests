8540fa4c136bc539c449a8786cffaa5341ca9db8
ansteisija-kat <katty93cat@gmail.com>
2025-02-20 22:34:30 +0300

    refactoring of api tests – more flexible test data & funcs

--- api_data/dogApi.js
+++ api_data/dogApi.js
@@ -13,6 +13,14 @@ export const SUBBREEDS = [
     { breed: 'dalmatian', subBreed: []}
 ];
 
+// макс. кол-во фоток породы, пока хранятся так для ручки нескольких картинок для контроля ограничений по кол-ву
+export const MAX_IMAGES = [
+    { breed: 'dalmatian', imagesMaxCount: 2 },
+    { breed: 'danish', imagesMaxCount: 4 },
+    { breed: 'mastiff', imagesMaxCount: 40 },
+    { breed: 'bulldog', imagesMaxCount: 10 },
+];
+
 export function getRandomBreed(BREEDS) {
     const randomIndex = Math.floor(Math.random() * BREEDS.length);
     const breed = BREEDS[randomIndex];
@@ -21,10 +29,30 @@ export function getRandomBreed(BREEDS) {
 
 export const randomBreed = getRandomBreed(BREEDS);
 
+export function getRandomBreedForSubbreed(SUBBREEDS) {
+    const randomIndex = Math.floor(Math.random() * SUBBREEDS.length);
+    const breed = SUBBREEDS[randomIndex];
+    return breed
+    // внутри хранится 1 объект из SUBBREEDS: breed и соотв. список subBreed
+}
+
+export const randomBreedForSubbreed = getRandomBreedForSubbreed(SUBBREEDS);
+
+export function getRandomBreedForImageCount(MAX_IMAGES) {
+    const randomIndex = Math.floor(Math.random() * MAX_IMAGES.length);
+    const breedImages = MAX_IMAGES[randomIndex];
+    return breedImages
+    // внутри хранится 1 объект из MAX_IMAGES: breed и соотв. imagesCount
+}
+
+export const randomBreedImageCount = getRandomBreedForImageCount(MAX_IMAGES);
+
+
+// paths
 export const pathGetRandomImage = `${BASE_URL}/breeds/image/random`
 export const pathGetOneImageByBreed = `${BASE_URL}/breed/${randomBreed}/images/random`
-export const pathGetMultiImagesByBreed = `${BASE_URL}/breed/${BREEDS}/images/random/${BREEDS}`
-export const pathGetSubbreedByBreed = `${BASE_URL}/breed/${BREEDS}/list`
+export const pathGetMultiImagesByBreed = `${BASE_URL}/breed/${randomBreedImageCount.breed}/images/random/${randomBreedImageCount.imagesMaxCount}`
+export const pathGetSubbreedByBreed = `${BASE_URL}/breed/${randomBreedForSubbreed.breed}/list`
 export const pathGetAllImagesByBreed = `${BASE_URL}/breed/${randomBreed}/images/`
 
 
--- tests/api/dogApi.test.js
+++ tests/api/dogApi.test.js
@@ -31,58 +31,45 @@ test(`get a 1 random image of breed`, async ({ request }) => {
 });
 
 
-[
-    { breed: 'mastiff', subBreed: [ 'bull', 'english', 'indian', 'tibetan' ]},
-    { breed: 'bulldog', subBreed: [ 'boston', 'english', 'french' ]},
-    { breed: 'bullterrier', subBreed: [ 'staffordshire' ] },
-    { breed: 'danish', subBreed: [ 'swedish' ]},
-    { breed: 'chow', subBreed: []},
-    { breed: 'dalmatian', subBreed: []},
-].forEach(({ breed, subBreed }) => {
-    test(`get a sub-breed by breed: the ${subBreed} ${breed}`, async ({ request }) => {
-        const response = await request.get(`${dogData.BASE_URL}/breed/${breed}/list`);
-        const responseData = await response.json();
+test('get sub-breeds by breed', async ({ request }) => {
+    const response = await request.get(dogData.pathGetSubbreedByBreed);
+    const subBreeds = dogData.randomBreedForSubbreed.subBreed;
 
-        await expect(response).toBeOK();
-        await expect(responseData).not.toBeNull();
-        await expect(responseData).toMatchObject(
-            expect.objectContaining({
-                status: 'success',
-                message: subBreed
-            }),
-        );
-    });
+    const responseData = await response.json();
+
+    await expect(response).toBeOK();
+    await expect(responseData).not.toBeNull();
+    await expect(responseData).toMatchObject(
+        expect.objectContaining({
+            status: 'success',
+            message: subBreeds
+        }),
+    );
+});
+
+
+test(`get multiple random images by breed (valid)`, async ({ request }) => {
+    const response = await request.get(dogData.pathGetMultiImagesByBreed);
+    const count = dogData.randomBreedImageCount.imagesMaxCount
+
+    const responseData = await response.json();
+
+    await expect(response).toBeOK();
+    await expect(responseData).not.toBeNull();
+    await expect(responseData).toMatchObject(
+        expect.objectContaining({
+            status: 'success',
+        }),
+        expect(responseData.message).toHaveLength(count)
+    );
 });
 
 
 [
-    { breed: 'dalmatian', imagesCount: 2 }, // макс. кол-во фоток породы
-    { breed: 'danish', imagesCount: 4 },  // макс. кол-во фоток породы
-    { breed: 'mastiff', imagesCount: 40 },
-    { breed: 'bulldog', imagesCount: 10 },
-    // тут оставляем параметры, потому что у пород разное макс. кол-во фоток
+    { breed: dogData.randomBreed, imagesCount: 0 },
+    { breed: dogData.randomBreed, imagesCount: null },
 ].forEach(({ breed, imagesCount }) => {
-    test(`get multiple random images by breed: whole ${imagesCount} ${breed}s (valid)`, async ({ request }) => {
-        const response = await request.get(`${dogData.BASE_URL}/breed/${breed}/images/random/${imagesCount}`);
-        const responseData = await response.json();
-
-        await expect(response).toBeOK();
-        await expect(responseData).not.toBeNull();
-        await expect(responseData).toMatchObject(
-            expect.objectContaining({
-                status: 'success',
-            }),
-            expect(responseData.message).toHaveLength(imagesCount)
-        );
-    });
-});
-
-
-[
-    { breed: 'dalmatian', imagesCount: 0 },
-    { breed: 'mastiff', imagesCount: null },
-].forEach(({ breed, imagesCount }) => {
-    test(`multiple random images by breed: whole ${imagesCount} ${breed}s (invalid, get 1 doggo anyway)`, async ({ request }) => {
+    test(`multiple random images by breed: whole ${imagesCount} doggos (invalid, get 1 doggo anyway)`, async ({ request }) => {
         const response = await request.get(`${dogData.BASE_URL}/breed/${breed}/images/random/${imagesCount}`);
         const responseData = await response.json();
 
